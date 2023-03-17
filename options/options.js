fd1d3b010e8f622ffc41533453e1e7df58db75d6
Crowdbotics <engineering@crowdbotics.com>
2023-03-17 18:08:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hesbon-t-14",
+  url: "https://hesbon-t-14.botics.co",
+  api: "https://hesbon-t-14.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
